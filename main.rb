aedc987ff70820fa97576a06aec9c437e4f655ce
Zac <thezacattacks@gmail.com>
2019-05-17 00:07:49 -0400

    coalesced the two get_quote functions
    added comments
    fixed sql statements to make further code simpler
    fixed Names/Categories arrays having blank entries
    fixed detection of !help

--- main.rb
+++ main.rb
@@ -17,42 +17,39 @@ SpecificStatement = Database.prepare('select text,
                                       page_title as title,
                                       page_link as link
                                       from quotes
-                                      where (character_name like ? or
-                                             character_category like ?)')
+                                      where group_id = (
+                                        select group_id from quotes
+                                          where (lower(character_name) = lower(?) or
+                                                 lower(character_category) = lower(?))
+                                        order by random() limit 1
+                                      )')
 ColorRegex = /<span style="color: #(?<color>.{6})">|<\/span>/
 Names = Database.execute('select distinct(character_name) as name
-                          from quotes').collect {|n| n['name']}
+                          from quotes')
+          .select  {|n| !n['name'].empty?}
+          .collect {|n| n['name']}
 Categories = Database.execute('select distinct(character_category) as category 
-                               from quotes').collect {|c| c['category']}
-NameRegex = /(?<name>#{Names.join('|')})/i
-CategoryRegex = /(?<category>#{Categories.join('|')})/i
+                               from quotes')
+               .select  {|c| !c['category'].empty?}
+               .collect {|c| c['category']}
+NameRegex = /(?<name>#{Names.map{|n| Regexp.quote(n) unless n.nil?}.join('|')})/i
+CategoryRegex = /(?<category>#{Categories.map{|c| Regexp.quote(c) unless c.nil?}.join('|')})/i
 HelpMsg = "I recognize these names: #{Names.join(', ')}\nAnd these categories: #{Categories.join(', ')}"
 
 
-# search for a certain name or group
-def get_specific_quote search
+# get a quote from our database
+#  if the search is nil we get a random quote,
+#  otherwise we search first and then get a random one from the results
+def get_quote search = nil
   character = ''
   link = ''
   title = ''
+
+  rows = search.nil? ?
+           RandomStatement.execute(rand MaxID) :
+           SpecificStatement.execute(search, search)
   
-  SpecificStatement.execute(search, search).collect.to_a.sample.collect do |quote|
-    character = quote['name']
-    link = "https://homestuck.com#{quote['link']}"
-    title = quote['title']
-    color = ColorRegex.match(quote['text'])
-    color = color.nil? ? 'ffffff' : color['color']
-    quote = "[hs][colorhex=#{color}]#{quote['text'].gsub(ColorRegex, '')}[/colorhex][/hs]"
-  end.join("\n")
-  { text: quote, character: character, link: link, title: title }
-end
-
-
-# get a generic random quote
-def get_random_quote
-  character = ''
-  link = ''
-  title = ''
-  quote = Statement.execute(rand MaxID).collect do |quote|
+  quote = rows.collect do |quote|
     character = quote['name']
     link = "https://homestuck.com#{quote['link']}"
     title = quote['title']
@@ -68,10 +65,8 @@ end
 #  reply with a random quote from that group
 #  or character
 Bot.on_reply do |bot, post|
-  next # until i sort out whats going on with regexing the post
-  if post.content.start_with?('!help')
-    bot.reply(HelpMsg,
-              spoiler: 'bot help post.content')
+  if post.content.gsub(/@\w+/, '').strip.start_with?('!help')
+    bot.reply(HelpMsg, spoiler: 'bot help')
   else
     hit = NameRegex.match(post.content) || CategoryRegex.match(post.content)
     
@@ -79,7 +74,7 @@ Bot.on_reply do |bot, post|
       bot.reply('I didn\'t recognize who that was. Reply with !help to get a list of names')
     else
       capture = hit.named_captures.keys.first
-      quote = get_specific_quote(hit[capture])
+      quote = get_quote(hit[capture])
       bot.reply_with_mentions("#{quote[:text]}\n\n#{quote[:link]}",
                               spoiler: quote[:title])
     end
@@ -88,7 +83,7 @@ end
 
 # get a quote and post it
 Bot.run do |bot|
-  quote = get_random_quote()
+  quote = get_quote
   bot.post("#{quote[:text]}\n\n#{quote[:link]}",
            spoiler: quote[:title])
 end
